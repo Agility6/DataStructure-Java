464c88871579a74ff5326f85b197a5d9c0c5c836
Agility6 <agility1013@gmail.com>
2024-03-20 17:33:02 +0800

    feat: avl add

---
+++ AVLTree/src/BinarySearchTree_AVL/AVLNode.java
@@ -0,0 +1,93 @@
+package BinarySearchTree_AVL;
+
+/**
+ * ClassName: BinarySearchTree_AVL.AVLNode
+ * Description:
+ *
+ * @Author agility6
+ * @Create 2024/3/19 16:38
+ * @Version: 1.0
+ */
+public class AVLNode {
+
+    public int element; // 值
+
+    public AVLNode left; // 左孩子
+
+    public AVLNode right; // 右孩子
+
+    public AVLNode parent; // 父节点
+
+    public int height = 1; // 记录节点的高度
+
+    /**
+     * 必须传入当前节点的值以及父节点
+     * @param element
+     * @param parent
+     */
+    public AVLNode(int element, AVLNode parent) {
+        this.element = element;
+        this.parent = parent;
+    }
+
+    public boolean isLeaf() {
+        return this.left == null && this.right == null;
+    }
+
+    public boolean NodeDegreeTwo() {
+        return this.left != null && this.right != null;
+    }
+
+    /**
+     * 计算该节点的平衡因子
+     * @param
+     * @return
+     */
+    public int balanceFactor() {
+        int leftHeight = left == null ? 0 : left.height;
+        int rightHeight = right == null ? 0 : right.height;
+        return leftHeight - rightHeight;
+    }
+
+    /**
+     * 返回子树高度较高的节点
+     * @return
+     */
+    public AVLNode tallerChild() {
+        int leftHeight = left == null ? 0 : left.height;
+        int rightHeight = right == null ? 0 : right.height;
+        if (leftHeight > rightHeight) return left;
+        if (rightHeight > leftHeight) return right;
+        return isLeftChild() ? left : right;
+    }
+
+    public void updateHeight() {
+        int leftHeight = left == null ? 0 : left.height;
+        int rightHeight = right == null ? 0 : right.height;
+        height = Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    /**
+     * 当前节点是否是左节点
+     * @return
+     */
+    public boolean isLeftChild() {
+        return parent != null && this.parent == left;
+    }
+
+    /**
+     * 当前节点是否是右节点
+     * @return
+     */
+    public boolean isRightChild() {
+        return parent != null && this.parent == right;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BinarySearchTree_AVL.AVLNode{" +
+                "element=" + element +
+                '}';
+    }
+}
---
+++ AVLTree/src/BinarySearchTree_AVL/AVLTree.java
@@ -0,0 +1,163 @@
+package BinarySearchTree_AVL;
+
+/**
+ * ClassName: BinarySearchTree_AVL.AVLTree
+ * Description:
+ *
+ * @Author agility6
+ * @Create 2024/3/19 16:37
+ * @Version: 1.0
+ */
+public class AVLTree {
+
+    private AVLNode root;
+
+    private int size;
+
+    public void add(int element) {
+
+        if (root == null) {
+            root = new AVLNode(element, null);
+            size++;
+
+            afterAdd(root);
+
+            return;
+        }
+
+        AVLNode node = root;
+        AVLNode insertNodeParent = root;
+        int cmp = 0;
+
+        while (node != null) {
+            cmp = cmp(node.element, element);
+            insertNodeParent = node;
+            if (cmp == 1) { // node.element > element 左边
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        AVLNode newCreateAVLNode = new AVLNode(element, insertNodeParent);
+        if (cmp == 1) insertNodeParent.left = newCreateAVLNode;
+        else insertNodeParent.right = newCreateAVLNode;
+
+        afterAdd(newCreateAVLNode);
+    }
+
+    /**
+     * 添加后进行平衡操作
+     * @param node
+     */
+    private void afterAdd(AVLNode node) {
+
+        if (isBalanced(node)) { // 平衡，更新高度
+            updateHeight(node);
+        } else { // 恢复平衡
+            rebalance(node);
+        }
+    }
+
+    /**
+     *
+     * @param g 高度最低的那个不平衡节点
+     */
+    private void rebalance(AVLNode g) {
+
+        AVLNode parent = g.tallerChild(); // p节点g子树较高的节点
+        AVLNode node = parent.tallerChild();
+
+        if (parent.isLeftChild()) { // p节点是g节点的左边 L
+            if (node.isLeftChild()) { // n节点是p节点的左边 L
+                rotateRight(g); // LL
+            } else { // n节点是p节点的右边 R
+                // LR
+                rotateLeft(parent);
+                rotateRight(g);
+            }
+        } else { // p节点是g节点的右边 R
+            if (node.isLeftChild()) { // n节点是p节点的左边 L
+                // RL
+                rotateRight(parent);
+                rotateLeft(g);
+            } else { // n节点是p节点的右边 R
+                rotateLeft(g); // RR
+            }
+        }
+
+    }
+
+    /**
+     * 右旋转
+     * @param g
+     */
+    private void rotateRight(AVLNode g) {
+        AVLNode parent = g.left;
+        AVLNode child = parent.right;
+        g.left = child; // g的左节点指向，parent节点的右节点
+        parent.right = g;
+        afterRotate(g, parent, child); // 统一处理其余指向
+    }
+
+    /**
+     * 左旋转
+     * @param g
+     */
+    private void rotateLeft(AVLNode g) {
+        AVLNode parent = g.right;
+        AVLNode child = parent.left;
+        g.right = child;
+        parent.left = g;
+        afterRotate(g, parent, child);
+    }
+
+    /**
+     * 更新g,parent,child指向
+     * @param g
+     * @param parent
+     * @param child
+     */
+    private void afterRotate(AVLNode g, AVLNode parent, AVLNode child) {
+
+        // 1. parent节点的父节点
+        parent.parent = g.parent;
+
+        // 2. 原先指向g的改为指向parent
+        if (g.isLeftChild()) { // 原先g是在左子树
+            g.parent.left = parent;
+        } else if (g.isRightChild()) { // 原先g是在右子树
+            g.parent.right = parent;
+        } else { // 原先g是root
+            root = parent;
+        }
+
+        // 3. 更新child的parent
+        if (child != null) {
+            child.parent = g;
+        }
+
+        // 4. 更新g的parent
+        g.parent = parent;
+
+        // 更新高度
+        updateHeight(g);
+        updateHeight(parent);
+
+    }
+
+
+    private void updateHeight(AVLNode node) {
+        node.updateHeight();
+    }
+
+
+    private int cmp(int e1, int e2) {
+        return Integer.compare(e1, e2);
+    }
+
+    private boolean isBalanced(AVLNode node) {
+        return Math.abs(node.left.height - node.right.height) <= 1;
+    }
+
+}
